80f4e26644ec8185e21fb8fc949936e03197ddf6
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2021-05-09 06:16:54 +0200

    Service | Add config testing

--- tests/unit/services/config-cat-test.ts
+++ tests/unit/services/config-cat-test.ts
@@ -4,9 +4,95 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | config-cat', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function (assert) {
+  test('it returns a config with a SDK key', function (assert) {
     const service = this.owner.lookup('service:config-cat');
-    assert.ok(service);
+    const config = service.getAddonConfig({ sdkKey: 'SDK_KEY' });
+    assert.strictEqual(config.mode, 'auto');
+  });
+
+  test('it returns a config with a default mode', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({});
+    assert.strictEqual(config.mode, 'auto');
+  });
+
+  test('it returns a config with the provided mode', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({ mode: 'lazy' });
+    assert.strictEqual(config.mode, 'lazy');
+  });
+
+  test('it returns a config with the local mode', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({ sdkKey: 'SDK_KEY', local: true });
+    assert.strictEqual(config.local, true);
+  });
+
+  test('it returns a config with the local mode when the SDK key is missing', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({});
+    assert.strictEqual(config.local, true);
+  });
+
+  test('it returns a config with a default empty poll options config', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({});
+    assert.deepEqual(config.options, {});
+  });
+
+  test('it returns a config with a filtered poll options config in lazy mode', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({
+      mode: 'lazy',
+      requestTimeoutMs: 400,
+      dataGovernance: 'Global',
+      maxInitWaitTimeSeconds: 300,
+      cacheTimeToLiveSeconds: 60,
+      pollIntervalSeconds: 120,
+    });
+
+    assert.deepEqual(config.options, {
+      requestTimeoutMs: 400,
+      dataGovernance: 'Global',
+      maxInitWaitTimeSeconds: 300,
+      cacheTimeToLiveSeconds: 60,
+    });
+  });
+
+  test('it returns a config with a filtered poll options config in auto mode', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({
+      mode: 'auto',
+      requestTimeoutMs: 400,
+      dataGovernance: 'Global',
+      maxInitWaitTimeSeconds: 300,
+      cacheTimeToLiveSeconds: 60,
+      pollIntervalSeconds: 120,
+    });
+
+    assert.deepEqual(config.options, {
+      requestTimeoutMs: 400,
+      dataGovernance: 'Global',
+      maxInitWaitTimeSeconds: 300,
+      pollIntervalSeconds: 120,
+    });
+  });
+
+  test('it returns a config with a filtered poll options config in manual mode', function (assert) {
+    const service = this.owner.lookup('service:config-cat');
+    const config = service.getAddonConfig({
+      mode: 'manual',
+      requestTimeoutMs: 400,
+      dataGovernance: 'Global',
+      maxInitWaitTimeSeconds: 300,
+      cacheTimeToLiveSeconds: 60,
+      pollIntervalSeconds: 120,
+    });
+
+    assert.deepEqual(config.options, {
+      requestTimeoutMs: 400,
+      dataGovernance: 'Global',
+      maxInitWaitTimeSeconds: 300,
+    });
   });
 });
