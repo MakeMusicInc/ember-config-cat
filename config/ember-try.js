e1daa27321bd5e58e5b2fb42569df6a0b85ebfc8
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2021-10-20 21:59:28 +0200

    Use ember-auto-import v2
    This reverts commit f5e39c6fbffd2e2f24dc8d927a89d14c6b27c876.

--- config/ember-try.js
+++ config/ember-try.js
@@ -42,10 +42,6 @@ module.exports = async function () {
       {
         name: 'ember-canary',
         npm: {
-          dependencies: {
-            'ember-auto-import': '^2',
-            webpack: '^5.55.1',
-          },
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
           },
